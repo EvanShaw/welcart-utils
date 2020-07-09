f85e223493fb2608b9a98f10ee1721c98e98a298
Evan Shaw <evandanielshaw@gmail.com>
2020-07-09 16:37:29 +0900

    feat: add isOrderEditPage to Pages utility class

---
+++ src/Pages.php
@@ -0,0 +1,26 @@
+<?php
+namespace Collne\WelcartUtils;
+
+/**
+ * どのページなのかを判断する関数がまとめてるクラス
+ */
+class Pages {
+
+    /**
+     * 今にいるページは受注編集ページだったら`true`を返却
+     *
+     * @author Evan D Shaw <evandanielshaw@gmail.com>
+     * @return boolean
+     */
+    public static function isOrderEditPage() {
+        if (is_admin()) {
+            $page = isset($_REQUEST['page']) ? $_REQUEST['page'] : '';
+            $action = isset($_REQUEST['order_action']) ? $_REQUEST['order_action'] : '';
+            if (trim($page) === 'usces_orderlist' && ($action === 'edit')) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
